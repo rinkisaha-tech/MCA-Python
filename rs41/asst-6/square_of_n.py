5736a45ae6332ed7e6d42a7f04433dd2b84f3ed4
Rinki Saha <128579571+rinkisaha-tech@users.noreply.github.com>
2025-09-10 13:51:58 +0530

    Add files via upload

---
+++ rs41/asst-6/square_of_n.py
@@ -0,0 +1,32 @@
+# def square_of_num(num):
+#     return num**2
+
+# n = int(input("Enter a number: "))
+# print("Square of the number is: ", square_of_num(n))
+
+
+#Area of a rectangle: 
+
+# def area_of_rect(l,b=5):
+#     return l*b
+
+# length = int(input("Enter the length: "))
+# print("Area : ", area_of_rect(length))
+
+# Printing emp details using keyword arguments: 
+
+# def emp_details(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"{key} : {value} ")
+
+
+# emp_details(Name = "Abc", Emp_id = 6, Gross_pay = 25000)
+# emp_details(Name = "xde", Emp_id = 8)
+
+
+# # sum and product of two given number: 
+# def s_p(a,b):
+#     return a+b, a*b
+
+# x = s_p(6,6)
+# print(f"Sum = {x[0]}, \nProduct = {x[1]}")
